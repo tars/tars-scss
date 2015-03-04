b89257622baabc0e0d86b1712e229f2d2226f2e9
Artem Malko <artem.malko@gmail.com>
2015-03-05 00:54:17 +0600

    remove css for ie9 task, move it to main css

--- tars/tasks/compile-css.js
+++ tars/tasks/compile-css.js
@@ -5,6 +5,7 @@ var gutil = require('gulp-util');
 var gulpif = require('gulp-if');
 var autoprefixer = require('gulp-autoprefixer');
 var replace = require('gulp-replace-task');
+var addsrc = require('gulp-add-src');
 var notify = require('gulp-notify');
 var tarsConfig = require('../../../tars-config');
 var notifier = require('../../helpers/notifier');
@@ -19,6 +20,9 @@ var scssFilesToConcatinate = [
     ];
 
 var useAutoprefixer = false;
+var helperStream;
+var mainStream;
+var ie9Stream;
 
 if (tarsConfig.autoprefixerConfig) {
     useAutoprefixer = true;
@@ -38,8 +42,7 @@ scssFilesToConcatinate.push(
     './markup/' + tarsConfig.fs.staticFolderName + '/scss/common.scss',
     './markup/' + tarsConfig.fs.staticFolderName + '/scss/plugins/**/*.scss',
     './markup/' + tarsConfig.fs.staticFolderName + '/scss/plugins/**/*.css',
-    './markup/modules/*/*.scss',
-    './markup/' + tarsConfig.fs.staticFolderName + '/scss/etc/**/*.scss'
+    './markup/modules/*/*.scss'
 );
 
 /**
@@ -58,7 +61,17 @@ module.exports = function(buildOptions) {
     );
 
     return gulp.task('css:compile-css', function() {
-        return gulp.src(scssFilesToConcatinate)
+
+        helperStream = gulp.src(scssFilesToConcatinate);
+        mainStream = helperStream.pipe(addsrc.append('./markup/' + tarsConfig.fs.staticFolderName + '/scss/etc/**/*.scss'));
+        ie9Stream = helperStream.pipe(
+                                addsrc.append([
+                                        './markup/modules/*/ie/ie9.scss',
+                                        './markup/' + tarsConfig.fs.staticFolderName + '/scss/etc/**/*.scss'
+                                    ])
+                            );
+
+        mainStream
             .pipe(concat('main' + buildOptions.hash + '.css'))
             .pipe(replace({
                 patterns: patterns,
@@ -88,6 +101,29 @@ module.exports = function(buildOptions) {
             .pipe(browserSync.reload({stream:true}))
             .pipe(
                 notifier('Scss-files\'ve been compiled')
+            );
+
+        return ie9Stream
+            .pipe(concat('main_ie9' + buildOptions.hash + '.css'))
+            .pipe(replace({
+                patterns: patterns,
+                usePrefix: false
+            }))
+            .pipe(sass({
+                errLogToConsole: false,
+                onError: function(error) {
+                    notify().write('\nAn error occurred while compiling css for ie9.\nLook in the console for details.\n');
+                    return gutil.log(gutil.colors.red(error.message + ' on line ' + error.line + ' in ' + error.file));
+                }
+            }))
+            .pipe(autoprefixer('ie 9', { cascade: true }))
+            .on('error', notify.onError(function (error) {
+                return '\nAn error occurred while autoprefixing css.\nLook in the console for details.\n' + error;
+            }))
+            .pipe(gulp.dest('./dev/' + tarsConfig.fs.staticFolderName + '/css/'))
+            .pipe(browserSync.reload({stream:true}))
+            .pipe(
+                notifier('Css-files for ie9 have been compiled')
             );
         });
 };
